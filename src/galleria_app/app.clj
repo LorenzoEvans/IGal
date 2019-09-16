e8a515e6b32715326a6175c0394389e2507de3ce
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-09-15 23:55:05 -0400

    Init commit.

---
+++ src/galleria_app/app.clj
@@ -0,0 +1,22 @@
+(ns galleria-app.app
+  (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
+            [compojure.handler :refer [site]]
+            [compojure.route :as route]
+            [clojure.java.io :as io]
+            [ring.adapter.jetty :as jetty]
+            [environ.core :refer [env]]))
+
+(defn welcome []
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello from the Galleria"})
+
+(defroute app-routes
+          (GET "/" []
+               (welcome))
+          (ANY "*" []
+               (route/not-found (slurp (io/resource "404.html")))))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 500))]
+    (jetty/run-jetty (site #'app-routes) {:port port :join? false})))
