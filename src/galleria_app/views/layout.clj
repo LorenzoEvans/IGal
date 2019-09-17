26671d15ae479e260098017c79c0f7f066b3a327
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-09-17 18:30:57 -0400

    Adding cdn networks for ui frameworks.

--- src/galleria_app/views/layout.clj
+++ src/galleria_app/views/layout.clj
@@ -1,1 +1,12 @@
-(ns galleria-app.views.layout)
+(ns galleria-app.views.layout
+  (:require [hiccup.page :only (html5 include-css include-js)]))
+
+(defn application [title & content]
+  (html5
+   [:head
+    [:title title]
+    (include-css "https://unpkg.com/onsenui/css/onsenui.css")
+    (include-css "https://unpkg.com/onsenui/css/onsen-css-components.min.css")
+    (include-js "https://unpkg.com/onsenui/js/onsenui.min.js")
+    [:body
+     [:div {:class "container"} content]]]))
